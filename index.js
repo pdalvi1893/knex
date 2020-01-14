7125e8e9e334a53f6c3a5d0dee84b98948618726
Pankaj Dalvi <pankaj@tentwenty.me>
2020-01-14 09:59:51 -0800

    connection commit.

--- index.js
+++ index.js
@@ -1,28 +1,31 @@
-// var knex = require('knex')({
-//     // client: 'pg',
-//     // connection: {
-//     //   host : 'dp416311-003.dbaas.ovh.net',
-//     //   user : 'buzzz_io_strapi',
-//     //   password : '8gyzk42B',
-//     //   database : 'buzzz_io_strapi',
-//     //   connectTimeout: 90000
-//     // }
-//     client: 'mssql',
-//     connection: {
-//       host : '127.0.0.1',
-//     //   user : 'sa',
-//     //   password : '8gyzk42B',
-//       database : 'Sharepoint_19731',
-//       connectTimeout: 90000
-//     }
-//   });
+var knex = require('knex')({
+    client: 'pg',
+    connection: {
+      host : 'dp416311-003.dbaas.ovh.net',
+      user : 'buzzz_io_strapi',
+      password : '8gyzk42B',
+      database : 'buzzz_io_strapi',
+      connectTimeout: 90000
+    },
+    pool: {  //<- this is important
+      min : 0 
+    },
+    // client: 'mssql',
+    // connection: {
+    //   host : '127.0.0.1',
+    // //   user : 'sa',
+    // //   password : '8gyzk42B',
+    //   database : 'Sharepoint_19731',
+    //   connectTimeout: 90000
+    // }
+  });
 
-//   knex.raw("SELECT VERSION()").then(
-//     (version) => console.log((version[0][0]))
-// ).catch((err) => { console.log( err); throw err });
-//     // .finally(() => {
-//     //     knex.destroy();
-//     // });
+  // knex.raw("SELECT VERSION()")
+  //     .then((version) => console.log((version[0][0])))
+  //     .catch((err) => { console.log( err); throw err })
+  //     .finally(() => {
+  //         knex.destroy();
+  //     });
 
 // //   var test = knex.select().table('users-permissions_user')
 
@@ -31,29 +34,36 @@
 
 
 
-var knex = require('knex')({
-    client: 'mssql',
-    connection: {
-        server:'localhost',
-      connectionString: "Initial Catalog=SharePoint_19731;Data Source=.;Integrated Security=SSPI;"
-    }
+// var knex = require('knex')({
+//     client: 'mssql',
+//     connection: {
+//         server:'localhost',
+//       connectionString: "Initial Catalog=SharePoint_19731;Data Source=.;Integrated Security=SSPI;"
+//     }
+//   });
+
+//   knex.raw("SELECT VERSION()").then(
+//     (version) => console.log((version[0][0]))
+// ).catch((err) => { console.log( err); throw err });
+//     // .finally(() => {
+//     //     knex.destroy();
+// //     // });
+
+knex
+  .from('users-permissions_user')
+  .select('username')
+  .then((test)=>{
+    //console.log(test);    
   });
 
-  knex.raw("SELECT VERSION()").then(
-    (version) => console.log((version[0][0]))
-).catch((err) => { console.log( err); throw err });
-    // .finally(() => {
-    //     knex.destroy();
-//     // });
 
-
-//   knex('TextKnex').table('Products')
-//     .select('Products.Price as Price')
-//     .then((product) => {
-//       console.log('log product', product);
-//       console.dir('dir product', product);
-//       logger.info('Query Data: %j', product);
-//     })
-//     .finally(() => {
-//       knex.destroy();
-//     });
+  // knex('buzzz_io_strapi').table('users-permissions_user')
+  //   .select('Products.Price as Price')
+  //   .then((product) => {
+  //     // console.log('log product', product);
+  //     // console.dir('dir product', product);
+  //     // logger.info('Query Data: %j', product);
+  //   })
+  //   .finally(() => {
+  //     knex.destroy();
+  //   });
